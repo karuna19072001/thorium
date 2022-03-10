06b3703a7b99b3ddcb2c82582a2ad0fe7c2a3861
karuna19072001 <99881092+karuna19072001@users.noreply.github.com>
2022-03-10 21:04:26 +0530

    Update middleware.js

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -9,3 +9,17 @@ let mid1=function(req,res,next){
     }
 }
 module.exports.mid1 = mid1;
+
+
+let authorization = async function (req, res) {
+    let token = req.headers["x-auth-token"]
+    let decodedToken = jwt.verify(token, "functionUp-thorium");
+    let usedLoggedIn = decodedToken.userId 
+    let param_Id = req.params.user_Id
+    if(usedLoggedIn !== param_Id)
+    return res.send({msg : "you are not authorized to access"});
+    next()
+}
+
+module.exports.mid1 = mid1;
+module.exports.authorization = authorization;
